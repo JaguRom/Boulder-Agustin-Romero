ee6f9c72cbd9e1e3726f7f4ab683291f0c3e5ad2
JaguRom <romerojuanagustin10@gmail.com>
2022-02-04 20:11:39 -0300

    Entrega 3 y 4

---
+++ src/Components/CartWidget/CartWidget.js
@@ -0,0 +1,16 @@
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
+const element = <FontAwesomeIcon icon={faShoppingCart} />
+
+
+
+function CartWidget() {
+  return (
+element
+  );
+}
+
+export default CartWidget;
+<i class="fas fa-shopping-cart"></i>
