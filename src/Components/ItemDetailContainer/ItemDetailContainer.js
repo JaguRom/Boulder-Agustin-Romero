a95b24b9ef826baa758f869c27baedc953a36bff
JaguRom <romerojuanagustin10@gmail.com>
2022-03-09 19:11:49 -0300

    firebase II

--- src/Components/ItemDetailContainer/ItemDetailContainer.js
+++ src/Components/ItemDetailContainer/ItemDetailContainer.js
@@ -3,6 +3,8 @@ import ItemDetail from "../ItemDetail/ItemDetail";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from 'react'
 import { getItems } from "../ItemArray/ItemArray";
+import {getDoc, doc, DocumentReference} from "firebase/firestore";
+import {firestoreDb} from "../../Services/Firebase/firebase";
 
 function ItemDetailContainer() {
   //state
@@ -15,7 +17,26 @@ function ItemDetailContainer() {
 
 
   useEffect(() => {
-      getItems(productId).then((product) => {
+setLoading(true);
+
+const docRef = doc(firestoreDb, "products", productId);
+getDoc(docRef).then(response => {
+  const product = {id: response.id , ...response.data()}
+  setProduct(product);
+}).finally(() => {
+  setLoading(false);
+})
+
+return (() => {
+  setProduct()
+})
+
+}, [productId]) // eslint-disable-line
+
+console.log("This is the Firebase product", product);
+
+
+    /*       getItems(productId).then((product) => {
           setProduct(product);
           console.log(product)
       })
@@ -25,7 +46,7 @@ function ItemDetailContainer() {
       .finally(() => {
         setLoading(false);
       })
-  },[productId])
+  },[productId]) */
 
 return (
       <div className="centered-flex">
--- src/Components/ItemListContainer/ItemListContainer.js
+++ src/Components/ItemListContainer/ItemListContainer.js
@@ -2,36 +2,37 @@ import ItemList from "../ItemList/ItemList";
 import { useEffect, useState } from 'react'
 import { getProducts, getProductsByCategory } from "../ItemArray/ItemArray";
 import { useParams } from "react-router-dom";
-import {getDocs, collection} from "firebase/firestore";
+import {getDocs, collection, query, where} from "firebase/firestore";
 import {firestoreDb} from "../../Services/Firebase/firebase";
  
 
-function ItemListContainer({
-  greeting
-}) {
+function ItemListContainer({greeting}) {
   //state
   const [products, setProducts] = useState([]);
   //loading
   const [loading, setLoading] = useState(true);
   //useParams
   const  {categoryId}  = useParams();
-
+  
 
   useEffect(() => {
-    if (categoryId) {
-      setTimeout(() => {
-        getProductsByCategory(categoryId).then((products) => {
-          setProducts(products);
-          setLoading(false);
-        });
-      }, 2000);
-    } else {
-      getProducts().then((products) => {
-        setProducts(products);
-        setLoading(false);
-      });
-    }
-  }, [categoryId]);
+    setLoading(true); 
+    const collectionRef = categoryId ?   
+    query (collection(firestoreDb, "products"), where ("category", "==", categoryId)) :
+    collection (firestoreDb, "products");
+ 
+ getDocs(collectionRef).then(QuerySnapshot => {
+   const products = QuerySnapshot.docs.map(doc => {
+     console.log("This is the doc", doc);
+    return {id: doc.id, ...doc.data()}
+    })
+    console.log("This is the products", products);
+    setProducts(products);
+    }).finally(() => {
+      setLoading(false);
+    })
+  }, [categoryId])
+  
 
 
 return (
@@ -46,5 +47,23 @@ return (
       </div>
     );
   }
+
   export default ItemListContainer;
 
+//Old Code
+  /*const productcsCollectionRef =  collection
+   */
+    /*    if (categoryId) {
+      setTimeout(() => {
+        getProductsByCategory(categoryId).then((products) => {
+          setProducts(products);
+          setLoading(false);
+        });
+      }, 2000);
+    } else {
+      getProducts().then((products) => {
+        setProducts(products);
+        setLoading(false);
+      });
+    }
+  }, [categoryId]); */
